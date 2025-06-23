ef8751e4bdb8d6a5a707cfaed0ef76742ecb07b7
totrieuvy <vytotrieu12a121@gmail.com>
2025-06-23 15:13:47 +0700

    fix cors

--- config/swagger.js
+++ config/swagger.js
@@ -11,7 +11,8 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        // url: "http://localhost:3000",
+        url: "https://prm392-finalproject.onrender.com"
         description: "Local server",
       },
     ],
